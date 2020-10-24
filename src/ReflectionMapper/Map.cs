8ac7efccc007c5592cd26358f19f1466e870aa59
Alexander Bleissem <alexander@bleissem.com>
2020-10-24 17:35:53 +0200

    delete "old" MapConfig

--- src/ReflectionMapper/Map.cs
+++ src/ReflectionMapper/Map.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ReflectionMapper
 {
-    public abstract class Map<MapTo, MapFrom> : MapConfig<MapTo, MapFrom> where MapTo : class, IMap<MapFrom>
+    public abstract class Map<MapTo, MapFrom> : IMap<MapFrom>
+        where MapTo : class
         where MapFrom : class
     {
+        public virtual Action<IMappingExpression<MapFrom, MapTo>> AlterReadMapping()
+        {
+            return null;
+        }
+
+        public virtual Action<IMappingExpression<MapTo, MapFrom>> AlterSaveMapping()
+        {
+            return null;
+        }
     }
 }
