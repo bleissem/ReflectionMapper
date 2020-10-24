511f23897f72c98b1b33f96edec3dd2d15d6e524
Alexander Bleissem <alexander@bleissem.com>
2020-10-24 15:46:12 +0200

    add tests

--- src/ReflectionMapper.Tests/UnitTest1.cs
+++ src/ReflectionMapper.Tests/UnitTest1.cs
@@ -8,7 +8,18 @@ namespace ReflectionMapper.Tests
         [Fact]
         public void Test1()
         {
+            AutoMapper.Mapper mapper = new AutoMapper.Mapper(new AutoMapper.MapperConfiguration(config =>
+            {
+                config.AddProfile<TestProfile>();
+            }));
 
+            MapFrom mapFrom = new MapFrom()
+            {
+                Id = 3
+            };
+
+            var mapto = mapper.Map<MapTo>(mapFrom);
+            Assert.Equal(3, mapto.Id);
         }
     }
 }
